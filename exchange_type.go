6fb0a611e021478935b7863dea5ee88bcee0ea62
harpy-wings <hassaniyaqoti@gmail.com>
2023-01-20 22:49:16 +0330

    feat: define exchange type strings

--- exchange_type.go
+++ exchange_type.go
@@ -2,7 +2,6 @@ package messagingwrapper
 
 type ExchangeType int
 
-// "direct", "fanout", "topic" and "headers".
 const (
 	ExchangeType_UNKNOWN = iota
 	ExchangeType_DIRECT
@@ -10,3 +9,39 @@ const (
 	ExchangeType_TOPIC
 	ExchangeType_HEADERS
 )
+
+var (
+	_ExchangeType2String = map[ExchangeType]string{
+		ExchangeType_UNKNOWN: "unknown",
+		ExchangeType_DIRECT:  "direct",
+		ExchangeType_FANOUT:  "fanout",
+		ExchangeType_TOPIC:   "topic",
+		ExchangeType_HEADERS: "headers",
+	}
+
+	_String2ExchangeType = map[string]ExchangeType{
+		"direct":  ExchangeType_DIRECT,
+		"fanout":  ExchangeType_FANOUT,
+		"topic":   ExchangeType_TOPIC,
+		"headers": ExchangeType_HEADERS,
+	}
+)
+
+func (e ExchangeType) String() string {
+	s, ok := _ExchangeType2String[e]
+	if !ok {
+		return "unknown"
+	}
+	return s
+}
+
+//ExchangeTypeFromString gets the ExchangeType value from a string
+//
+// "direct", "fanout", "topic" and "headers".
+func (e ExchangeType) ExchangeTypeFromString(s string) ExchangeType {
+	e, ok := _String2ExchangeType[s]
+	if !ok {
+		return ExchangeType_UNKNOWN
+	}
+	return e
+}
